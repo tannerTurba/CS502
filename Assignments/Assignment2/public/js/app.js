b489d042accd8b5ac0409dddd2cf2a777d28e983
Tanner Turba <turba7166@uwlax.edu>
2024-02-17 02:22:08 -0600

    menu page working
    Signed-off-by: Tanner Turba <turba7166@uwlax.edu>

---
+++ Assignments/Assignment2/public/js/app.js
@@ -0,0 +1,211 @@
+$(document).ready(async function() {
+    let sid = await getSID();
+    let metadata = await getMetadata();
+  
+    // Set font options using metadata.
+    metadata.fonts.forEach((font) => {
+      $('#font').append($('<option>', {
+        value: font.family, 
+        text: font.family,
+        id: font.family
+      }));
+      $(`option[value='${font.family}']`).css("font-family", `'${font.family}', ${font.category}`);
+      $('head').append(`<link href="${font.url}" rel="stylesheet">`);
+    });
+  
+    // Set level options using metadata.
+    metadata.levels.forEach((level) => {
+      $('#level').append($('<option>', {
+        value: level.name, 
+        text: level.name
+      }));
+    });
+  
+    // Set default values using metadata.
+    $('#word').val(metadata.defaults.colors.wordColor);
+    $('#guess').val(metadata.defaults.colors.guessColor);
+    $('#fore').val(metadata.defaults.colors.foreColor);
+    $('#font').val(metadata.defaults.font.family);
+    $('#level').val(metadata.defaults.level.name);
+
+    await updateView();
+  });
+
+  // $('#newGameBtn').on('click', async function() {
+  //   alert("btn clicked");
+  // });
+  
+  async function newGameBtnClick() {
+    await createGame();
+    await updateView();
+  }
+
+  async function updateView() {
+    // Populate game history.
+    let historyHTML = "";
+    let allGames = await getAllGames();
+    if (!allGames.msg) {
+      allGames.forEach((game) => {
+        let html = historicalGameHTML(game);
+        historyHTML = historyHTML.concat(html);
+      });
+    }
+    $('#history').append(historyHTML);
+  }
+
+  function historicalGameHTML(game) {
+    const level = game.level.name;
+    const remaining = game.remaining;
+    const answer = game.target;
+    const status = game.status;
+    let phrase = "";
+    game.view.split("").forEach((letter) => {
+      phrase = phrase.concat(`<p class="phrase" style="background-color: ${game.colors.wordColor}; color: ${game.colors.foreColor}">${letter.toUpperCase()}</p>`);
+    });
+
+    return `<div class="row my-3">
+    <div class="col-1">
+        ${level}
+    </div>
+    <div class="col-6 d-inline-flex">
+        ${phrase}
+    </div>
+    <div class="col-2">
+        ${remaining}
+    </div>
+    <div class="col-2" ${status === "unfinished" ? "style=\"visibility: hidden;\"" : ""}>
+        ${answer}
+    </div>
+    <div class="col-1">
+        ${status}
+    </div>
+    </div>\n`;
+  }
+  
+  async function getSID() {
+    return fetch('/api/v1/sid')
+      .then(response => {
+        if (!response.ok) {
+          console.log('Failed to get SID');
+        }
+        return response.json();
+      })
+      .then(data => {
+        return data.sid;
+      })
+      .catch(error => {
+        console.error('Error fetching /api/v1/sid: ', error);
+      });
+  }
+  
+  async function getMetadata() {
+    return fetch('/api/v1/meta')
+      .then(response => {
+        if (!response.ok) {
+          console.log('Failed to get Metadata!');
+        }
+        return response.json();
+      })
+      .then(data => {
+        return JSON.parse(data);
+      })
+      .catch(error => {
+        console.error('Error fetching /api/v1/meta: ', error);
+      });
+  }
+  
+  async function getAllGames() {
+    const sessionID = await getSID();
+    let games = await fetch(`/api/v1/${sessionID}/games`)
+      .then(response => {
+        if (!response.ok) {
+          console.log(`Failed to get games for session ${sessionID}!`);
+        }
+        return response.json();
+      })
+      .then(data => {
+
+        return JSON.parse(data);
+      })
+      .catch(error => {
+        console.error('Error fetching /api/v1/:sid/games: ', error);
+      });
+      return games;
+  }
+  
+  async function createGame() {
+    // Get game config.
+    const sessionID = await getSID();
+    const level = document.getElementById('level').value;
+    const font = document.getElementById('font').value;
+    
+    // Color data:
+    const wordColor = document.getElementById('word').value;
+    const guessColor = document.getElementById('guess').value;
+    const foreColor = document.getElementById('fore').value;
+    const colorObj = {
+      guessColor: guessColor,
+      foreColor: foreColor,
+      wordColor: wordColor
+    };
+  
+    let game = await fetch (`/api/v1/${sessionID}/games?level=${level}`, {
+      method: 'POST',
+      headers: {
+        'X-font' : font, 
+        'Content-Type' : 'application/json'
+      },
+      body: JSON.stringify(colorObj)
+    })
+      .then(response => {
+        if (!response.ok) {
+          console.log(`Failed to create a game for session '${sessionID}'.`);
+        }
+        return response.json();
+      })
+      .then(data => {
+        return JSON.parse(data);
+      })
+      .catch(error => {
+        console.error(`Error fetching /api/v1/:sessionID/games: `, error);
+      })
+      return game;
+  }
+  
+  async function getGame(gameID) {
+    const sessionID = await getSID();
+  
+    return fetch(`/api/v1/${sessionID}/games/${gameID}`)
+      .then(response => {
+        if (!response.ok) {
+          console.log(`Failed to get game '${gameID}' for session '${sessionID}'.`);
+        }
+        return response.json();
+      })
+      .then(data => {
+        return JSON.parse(data);
+      })
+      .catch(error => {
+        console.error('Error fetching /api/v1/:sessionID/games/:gameId: ', error);
+      });
+  } 
+  
+  async function makeGuess(gameID, letter) {
+    const sessionID = await getSID();
+  
+    return fetch(`/api/v1/${sessionID}/games/${gameID}/guesses?guess=${letter}`, {
+      method: 'POST'
+    })
+      .then(response => {
+        if (!response.ok) {
+          console.log(`Failed to make a guess in game '${gameID}' for '${sessionID}'.`);
+        }
+        return response.json();
+      })
+      .then(data => {
+        return JSON.parse(data);
+      })
+      .catch(error => {
+        console.error(`Error fetching /api/v1/sessionID/games/gameID: `, error);
+      });
+  }
---
+++ Assignments/Assignment2/routes/index.js
@@ -0,0 +1,352 @@
+var fs = require('fs');
+var express = require('express');
+var router = express.Router();
+
+/**
+ * The class to represent a Font object.
+ */
+class Font {
+    /**
+     * Creates a Font object.
+     * @param {string} category The category the font belongs to.
+     * @param {string} family the family the font belongs to.
+     * @param {string} rule the rule to use.
+     * @param {string} url the font's style url.
+     */
+    constructor(category, family, rule, url) {
+      this.category = category;
+      this.family = family;
+      this.rule = rule;
+      this.url = url;
+    }
+  }
+  /**
+   * Predefined Font objects, which represent the supported fonts.
+   */
+  Font.FONTS = {
+    PROTEST_RIOT: new Font("sans-serif", "Protest Riot", "regular", "https://fonts.googleapis.com/css2?family=Protest+Riot&display=swap"),
+    ROBOTO: new Font("sans-serif", "Roboto", "regular", "https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"),
+    NOTO_SERIF: new Font("serif", "Noto Serif", "normal", "https://fonts.googleapis.com/css2?family=Noto+Serif:ital,wght@0,100..900;1,100..900&display=swap")
+  };
+  
+  /**
+   * The class to represent a Level object.
+   */
+  class Level {
+    /**
+     * Creates a Level object.
+     * @param {number} rounds The number of rounds to play.
+     * @param {number} minLength The minimum length of the word.
+     * @param {number} maxLength The maximum length of the word.
+     * @param {string} name The name identifier of a level.
+     */
+    constructor(rounds, minLength, maxLength, name) {
+      this.rounds = rounds;
+      this.minLength = minLength;
+      this.maxLength = maxLength;
+      this.name = name;
+    }
+  }
+  /**
+   * Predefined Level objects, which represent the supported levels.
+   */
+  Level.LEVELS = {
+    EASY: new Level(8, 3, 5, "Easy"),
+    MEDIUM: new Level(7, 4, 10, "Medium"),
+    HARD: new Level(6, 9, 300, "Hard")
+  };
+  
+  /**
+   * The class to represent a Color object.
+   */
+  class Colors {
+    /**
+     * Creates a Color object.
+     * @param {string} guessColor the color of the guess background.
+     * @param {string} foreColor the color of the text background.
+     * @param {string} wordColor the color of the text.
+     */
+    constructor(guessColor, foreColor, wordColor) {
+      this.guessColor = guessColor;
+      this.foreColor = foreColor;
+      this.wordColor = wordColor;
+    }
+  }
+  /**
+   * A predefined Color object that defines the default color values.
+   */
+  Colors.DEFAULT = new Colors("#fefae0", "#606c38", "#283618");
+  
+  /**
+   * The class to represent a Metadata object.
+   */
+  class Metadata {
+    /**
+     * Creates a Metadata object.
+     * @param {List<Font>} fonts The fonts supported by the router.
+     * @param {List<Level>} levels The levels supported by the router.
+     * @param {Defaults} defaults The default settings of the router.
+     */
+    constructor(fonts, levels, defaults) {
+      this.fonts = fonts;
+      this.levels = levels;
+      this.defaults = defaults;
+    }
+  }
+  
+  /**
+   * The class to represent a Defaults object.
+   */
+  class Defaults {
+    /**
+     * 
+     * @param {Font} font The default Font configuration.
+     * @param {Level} level The default Level configuration.
+     * @param {Colors} colors The default Colors configuration.
+     */
+    constructor(font, level, colors) {
+      this.font = font;
+      this.level = level;
+      this.colors = colors;
+    }
+  }
+  
+  /**
+   * The class to represent a Game object.
+   */
+  class Game {
+    /**
+     * Creates a Game object.
+     * @param {Colors} colors The Colors configuration.
+     * @param {Font} font The Font configuration.
+     * @param {string} id A unique identifier for the game.
+     * @param {Level} level The Level configuration.
+     */
+    constructor(colors, font, id, level, target) {
+      this.colors = colors;
+      this.font = font;
+      this.guesses = "";
+      this.id = id;
+      this.level = level;
+      this.remaining = this.level.rounds;
+      this.status = Game.STATUSES.UNFINISHED;
+      this.target = target;
+      this.timestamp = Date.now();
+      this.timeToComplete = "";
+      this.view = "".padStart(target.length, '_');
+    }
+    /**
+     * Adds a guess to the list of guessed letters.
+     */
+    addGuess = new function(guess) {
+        if (this.guesses === undefined) {
+            this.guesses = guess;
+        }
+        else {
+            this.guesses = this.guesses.concat(guess);
+        }
+    }
+    
+    /**
+     * The possible statuses that the game can be. 
+     */
+    static STATUSES = {
+        UNFINISHED: 'unfinished',
+        LOSS: 'loss',
+        VICTORY: 'victory'
+    };
+  }
+  
+  /**
+   * The class to represent an Error object.
+   */
+  class Error {
+    /**
+     * Creates an Error object.
+     * @param {string} msg The error message.
+     */
+    constructor(msg) {
+      this.msg = msg;
+    }
+  }
+  
+const sessions = new Map();
+const fonts = [Font.FONTS.PROTEST_RIOT, Font.FONTS.ROBOTO, Font.FONTS.NOTO_SERIF];
+const levels = [Level.LEVELS.EASY, Level.LEVELS.MEDIUM, Level.LEVELS.HARD];
+
+async function getWordFromList(min, max) {
+    return new Promise((resolve, reject) => {
+        fs.readFile('./public/wordlist.txt', 'utf8', (err, data) => {
+            if (err) {
+                reject(err)
+            }
+            else {
+                let allWords = data.split(/\s+/);
+                let result = allWords.filter((word) => {
+                    return min <= word.length && word.length <= max
+                });
+                let index = Math.floor(Math.random() * result.length);
+                let randomWord = result[index];
+                resolve(randomWord);
+            }
+        });
+    });
+}
+
+/* GET home page. */
+router.get('/', (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+/* GET SID. */
+router.get('/api/v1/sid', (req, res) => {
+  if (req.sessionID) {
+    res.status(200).json({ sid: req.sessionID });
+  }
+  else {
+    res.status(500).json({ error: 'No SID available' });
+  }
+});
+
+/* GET a metadate obj describing all user-configurable settings. */
+router.get('/api/v1/meta', (req, res) => {
+  // Create the default config for the metadate and respond.
+  const defaults = new Defaults(Font.FONTS.NOTO_SERIF, Level.LEVELS.MEDIUM, Colors.DEFAULT);
+  const metadata = new Metadata(fonts, levels, defaults)
+  res.status(200).json( JSON.stringify(metadata) );
+});
+
+/* GET a list of supported fonts. */
+router.get('/api/v1/fonts', (req, res) => {
+  res.status(200).json( JSON.stringify(fonts) );
+});
+
+/* GET a list of games associated with the sid. */
+router.get('/api/v1/:sid/games', (req, res) => {
+    const sid = req.params.sid;
+    const games = sessions.get(sid);
+    
+    if (games) {
+        res.status(200).json( JSON.stringify(Array.from(games.values())) );
+    }
+    else {
+        res.status(200).json( JSON.stringify(new Error(`No games associated with '${sid}'`)) );
+    }
+});
+
+/* GET the game associated with sid and gid. */
+router.get('/api/v1/:sid/games/:gid', (req, res) => {
+  const sid = req.params.sid;
+  const gid = req.params.gid;
+  const games = sessions.get(sid);
+  
+  // Error checking in case session or game do not exist.
+  if (games) {
+    const game = games.get(gid);
+    if (game) {
+      res.status(200).json( JSON.stringify(game) );
+    }
+    else {
+      const error = new Error(`The game '${gid}' is not associated with the session '${sid}'.`)
+      res.status(200).json( JSON.stringify(error) );
+    }
+  }
+  const error = new Error(`The session '${sid}' does not exist.`)
+  res.status(200).json( JSON.stringify(error) );
+});
+
+/* POST a new game object assciated with the sid. */
+router.post('/api/v1/:sid/games', async (req, res) => {
+  // Get sessionID and the game obj from the request.
+  const sid = req.params.sid;
+  let level;
+  if (req.query.level === "Easy") {
+    level = Level.LEVELS.EASY;
+  } 
+  else if (req.query.level === "Medium") {
+    level = Level.LEVELS.MEDIUM;
+  }
+  else {
+    level = Level.LEVELS.HARD;
+  }
+  const font = req.headers['x-font'];
+  const color = new Colors(req.body.guessColor, req.body.foreColor, req.body.wordColor);
+  let targetWord = await getWordFromList(level.minLength, level.maxLength);
+  targetWord = targetWord.toUpperCase();
+
+  let games;
+
+  if (sessions.has(sid)) {
+    // Get the map of games already associated to the sessionID. 
+    games = sessions.get(sid);
+  }
+  else {
+    // Create a map of games associated with the sessionID.
+    games = new Map();
+    sessions.set(sid, games);
+  }
+
+  console.log(`COLOR: ${JSON.stringify(color)}`);
+  console.log(JSON.stringify(await req.body));
+  console.log(await req.body.guessColor);
+  console.log(await req.body.foreColor);
+  console.log(await req.body.wordColor);
+  
+  // Get the new id, create the game, add to the map of games.
+  let id = games.length;
+  const newGame = new Game(color, font, id, level, targetWord);
+  games.set(id, newGame);
+  res.status(200).json( JSON.stringify(newGame) );
+});
+
+/* POST a new guess object. */
+router.post('/api/v1/:sid/games/:gid/guesses', (req, res) => {
+  const sid = req.params.sid;
+  const gid = req.params.gid;
+  const guess = req.query['guess'].toUpperCase();
+  let error;
+
+  // Error checking in case session or game does not exist.
+  const games = sessions.get(sid);
+  if (games) {
+    const game = games.get(gid);
+    if (game) {
+        if (game.guesses.includes(guess)) {
+            /* Do nothing... */
+        }
+        else if (game.target.includes(guess)) {
+            game.addGuess(guess);
+            let newView = "";
+            for (let i = 0; i < game.target.length; i++) {
+                if (guess === game.target[i]) {
+                    newView.concat(guess);
+                }
+                else {
+                    newView.concat('_');
+                }
+            }
+            game.view = newView;
+        }
+        else {
+            game.addGuess(guess);
+            game.remaining--;
+        }
+
+        if (!game.view.includes('_')) {
+            game.status = game.STATUSES.VICTORY;
+        }
+        else if (game.remaining == 0) {
+            game.status = game.STATUSES.LOSS;
+        }
+        res.status(200).json( JSON.stringify(game) );
+    }
+    else {
+      error = new Error(`The game '${gid}' is not associated with the session '${sid}'.`)
+      res.status(200).json( JSON.stringify(error) );
+    }
+  }
+  error = new Error(`The session '${sid}' does not exist.`)
+  res.status(200).json( JSON.stringify(error) );
+});
+
+module.exports = router;
