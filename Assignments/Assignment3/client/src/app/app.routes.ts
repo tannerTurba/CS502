273425129104eaa183961ffa6f7053ca1c545e32
Tanner Turba <turba7166@uwlax.edu>
2024-04-02 16:07:53 -0500

    removing extra comments
    Signed-off-by: Tanner Turba <turba7166@uwlax.edu>

--- Assignments/Assignment3/client/src/app/app.routes.ts
+++ Assignments/Assignment3/client/src/app/app.routes.ts
@@ -7,8 +7,6 @@ import { LoginComponent } from './login/login.component';
 export const routes: Routes = [
     { path: '', redirectTo: '/login', pathMatch: 'full' },
     { path: 'login', title: 'Wheel of Fortuner - Login', component: LoginComponent },
-    // { path: '', redirectTo: 'games', pathMatch: 'full' },
-    // { path: 'games', title: 'Wheel of Fortuner - Game List', component: GameListComponent },
     { path: 'users/:uid/games', title: 'Wheel of Fortuner - Game List', component: GameListComponent },
     { path: 'users/:uid/games/:gid', title: 'Wheel of Fortuner - Game', component: GameComponent }
 ];
