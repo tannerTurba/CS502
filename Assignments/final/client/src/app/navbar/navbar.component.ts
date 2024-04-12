d35e8401ac2c9c14af95964829d4c8ee41f338a7
Tanner Turba <turba7166@uwlax.edu>
2024-04-11 22:54:58 -0500

    ...
    Signed-off-by: Tanner Turba <turba7166@uwlax.edu>

--- Assignments/final/client/src/app/navbar/navbar.component.ts
+++ Assignments/final/client/src/app/navbar/navbar.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { DropdownMenuComponent } from '../dropdown-menu/dropdown-menu.component';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [
-    RouterModule
+    RouterModule,
+    DropdownMenuComponent
   ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
