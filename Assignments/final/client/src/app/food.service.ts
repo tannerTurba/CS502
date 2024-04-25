6e44113d3e2943e7f48d0eed1b5603c81fa112d6
Tanner Turba <turba7166@uwlax.edu>
2024-04-25 18:03:13 -0500

    branching
    Signed-off-by: Tanner Turba <turba7166@uwlax.edu>

--- Assignments/final/client/src/app/food.service.ts
+++ Assignments/final/client/src/app/food.service.ts
@@ -16,37 +16,6 @@ export class FoodService {
 
   constructor(private http: HttpClient) { }
 
-  // keywordSearch(keyword: string): Observable<KeywordResult> {
-  //   return this.http.get<KeywordResult>(`${this.baseUrl}/parser`, {
-  //     params: {
-  //       'app_id': this.appId,
-  //       'app_key': this.appKey,
-  //       'ingr': keyword,
-  //       'nutrition-type': this.nutritionType
-  //     }
-  //   });
-  // }
-
-  // getNutrients(foodId: string): Observable<MoreNutrients> {
-  //   return this.http.post<MoreNutrients>(`${this.baseUrl}/nutrients`, 
-  //   {
-  //     "ingredients": [
-  //       {
-  //         "quantity": 1,
-  //         "measureURI": "http://www.edamam.com/ontologies/edamam.owl#Measure_serving",
-  //         "qualifiers": [],
-  //         "foodId": foodId
-  //       }
-  //     ]
-  //   },
-  //   {
-  //     params: {
-  //       'app_id': this.appId,
-  //       'app_key': this.appKey
-  //     }
-  //   });
-  // }
-
   keywordSearch(keyword: string): Observable<KeywordResult> {
     return this.http.get<KeywordResult>(`${this.apiVersion}/foods`, {
       params: {
