8e41af669954a59a0c0e5888af976efa3c4b859f
Tanner Turba <turba7166@uwlax.edu>
2024-04-24 20:31:57 -0500

    localized files and api
    Signed-off-by: Tanner Turba <turba7166@uwlax.edu>

--- Assignments/final/client/src/app/food.service.ts
+++ Assignments/final/client/src/app/food.service.ts
@@ -8,42 +8,54 @@ import { MoreNutrients } from './more-nutrients';
   providedIn: 'root'
 })
 export class FoodService {
-  baseUrl: string = 'https://api.edamam.com/api/food-database/v2';
-  appId: string = '35b313ca';
-  appKey: string = '5fe592b84538ad54e7a6d8f45f321e34';
-  nutritionType: string = 'cooking';
+  apiVersion: string = 'api/v1';
+  // baseUrl: string = 'https://api.edamam.com/api/food-database/v2';
+  // appId: string = '35b313ca';
+  // appKey: string = '5fe592b84538ad54e7a6d8f45f321e34';
+  // nutritionType: string = 'cooking';
 
   constructor(private http: HttpClient) { }
 
+  // keywordSearch(keyword: string): Observable<KeywordResult> {
+  //   return this.http.get<KeywordResult>(`${this.baseUrl}/parser`, {
+  //     params: {
+  //       'app_id': this.appId,
+  //       'app_key': this.appKey,
+  //       'ingr': keyword,
+  //       'nutrition-type': this.nutritionType
+  //     }
+  //   });
+  // }
+
+  // getNutrients(foodId: string): Observable<MoreNutrients> {
+  //   return this.http.post<MoreNutrients>(`${this.baseUrl}/nutrients`, 
+  //   {
+  //     "ingredients": [
+  //       {
+  //         "quantity": 1,
+  //         "measureURI": "http://www.edamam.com/ontologies/edamam.owl#Measure_serving",
+  //         "qualifiers": [],
+  //         "foodId": foodId
+  //       }
+  //     ]
+  //   },
+  //   {
+  //     params: {
+  //       'app_id': this.appId,
+  //       'app_key': this.appKey
+  //     }
+  //   });
+  // }
+
   keywordSearch(keyword: string): Observable<KeywordResult> {
-    return this.http.get<KeywordResult>(`${this.baseUrl}/parser`, {
+    return this.http.get<KeywordResult>(`${this.apiVersion}/foods`, {
       params: {
-        'app_id': this.appId,
-        'app_key': this.appKey,
-        'ingr': keyword,
-        'nutrition-type': this.nutritionType
+        'keyword': keyword
       }
     });
   }
 
   getNutrients(foodId: string): Observable<MoreNutrients> {
-    return this.http.post<MoreNutrients>(`${this.baseUrl}/nutrients`, 
-    {
-      "ingredients": [
-        {
-          "quantity": 1,
-          "measureURI": "http://www.edamam.com/ontologies/edamam.owl#Measure_serving",
-          "qualifiers": [],
-          "foodId": foodId
-        }
-      ]
-    },
-    {
-      params: {
-        'app_id': this.appId,
-        'app_key': this.appKey
-      }
-    });
+    return this.http.get<MoreNutrients>(`${this.apiVersion}/foods/${foodId}`);
   }
-
 }
